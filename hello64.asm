43c6bec1f39121ff701d3df63f38039ccdd9fe8d
g0tie <gaultierbegue@outlook.com>
2025-05-15 10:05:45 -0400

    correct code

--- hello64.asm
+++ hello64.asm
@@ -1,15 +1,21 @@
 SECTION .data
-msg2 db 'Hello World',0xA                            
-SECTION .bss
+	msg2 db 'Hello World',0xA                            
 
-                                                     SECTION .text
+SECTION .text
 global _start
-                                                     _start:
+
+_start:
+	jmp printHelloWorld
+	jmp _exit
+
+printHelloWorld:
         mov rax, 1
         mov rsi,msg2
         mov rdi,1
         mov rdx,12                                           
 	syscall                                              
-	mov rax,60
-        xor rdi,rdi
-        syscall
+
+_exit:
+	mov rax, 60
+	xor rdi, rdi
+	syscall
